97f64af3b1800d221a732b5f334c2e51147c36b1
Patrick Lewis <pllewis1@gmail.com>
2016-01-06 11:34:02 -0600

    sub page finished

--- src/js/components/subscription.js
+++ src/js/components/subscription.js
@@ -9,7 +9,7 @@ class Subscription extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      success: null,
+      error: null,
       disableButton: false,
       errorMsg: "",
       plan: ""
@@ -17,6 +17,7 @@ class Subscription extends React.Component {
     this.handlePaySubmit = this.handlePaySubmit.bind(this);
     this.stripeResponseHandler = this.stripeResponseHandler.bind(this);
     this.handleResults = this.handleResults.bind(this);
+    this.hideResults = this.hideResults.bind(this);
 
     Stripe.setPublishableKey('pk_test_eiTJaf9ETvML34B3RRoq2gRh');
   }
@@ -54,15 +55,21 @@ class Subscription extends React.Component {
     handleResults(results) {
       if (!results) {
         this.setState({
-          success: false
+          error: true
         });
 
       } else {
         this.setState({
-          success: true
+          error: false
         });
 
       }
+    }
+
+    hideResults(e) {
+      this.setState({
+        error: null
+      });
     }
 
 
@@ -108,10 +115,10 @@ render() {
       </div>
     )
   }
-  if (this.state.success) {
-    resultsMsg = <p>You're all set! <Link to="dashboard">Head to your Dashboard</Link></p>
-  } else if(this.state.success === false){
-    resultsMsg = <p>Oops, something flubbed! Try again. If the error persists, email us at support@snailephant.com and we can walk you through it.</p>
+  if (this.state.error === false) {
+    resultsMsg = <div className="successMsg"><button onClick={this.hideResults}>X</button><p>You're all set! <Link to="dashboard">Head to your Dashboard</Link></p></div>
+  } else if (this.state.error === true){
+    resultsMsg = <div className="failMsg"><button onClick={this.hideResults}>X</button><p>Oops, something flubbed! Try again. If the error persists, email us at support@snailephant.com and we can walk you through it.</p></div>
   }
 
     return (
@@ -119,7 +126,7 @@ render() {
         <div className="response" id="response"> { resultsMsg } </div>
         <span ref="payment-errors">{this.state.errorMsg}</span>
           <div className="form-row">
-          <h1>Choose Your SnailePlan</h1>
+          <h1>Choose Your Snailscription Plan</h1>
           <article className="plan" id="solo" onClick={this.handlePlanSelect.bind(this, 'solo')}>
             <p>solo</p>
             <label>Individual Card for $2</label>
