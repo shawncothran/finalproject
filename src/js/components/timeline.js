38a9a6641f878675c1e8800305b27c8e66286c06
Patrick Lewis <pllewis1@gmail.com>
2016-01-06 14:41:36 -0600

    card times done

--- src/js/components/timeline.js
+++ src/js/components/timeline.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import $ from 'jquery';
 import { Link } from 'react-router';
+import Moment from 'moment';
 
 import User from '../models/user';
+import Card from '../models/card';
 
 const API_ROOT = 'http://snailephant.herokuapp.com/cards/';
 
+
 class Timeline extends React.Component {
   render() {
+    console.log(Moment('1912-12-12').format("MMM Do YY"));
     let cards = this.props.cards.map(function (card) {
-      return <div className="wrap"><iframe className="frame" scrolling="no" key={card.id} src={API_ROOT + card.id} /></div>
+      console.log('hi from timeline', card);
+      return <div className="outerwrap"><div className="wrap"><iframe className="frame" scrolling="no" key={card.id} src={API_ROOT + card.id} /><date>{Moment(card.date).format("MMM Do YY")}</date></div></div>
     })
+    // Card.getDate();
 
     return (
       <section className="timeline">
